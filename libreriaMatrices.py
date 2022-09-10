bd68546f165deaefd9e5e17534d089a817c092cc
Sebastian Rodriguez <juan.rodriguez-pen@mail.escuelaing.edu.co>
2022-09-09 23:57:13 -0500

    matriz

--- libreriaMatrices.py
+++ libreriaMatrices.py
@@ -27,13 +27,15 @@ def add_matrices(a,b):
         resultado.append(add_vector(a,b))
     print(resultado)
 
-def inv_matriz(a):
+def mult_esc_matriz(matriz,a):
     resultado = []
-    
+    for i in range(len(matriz)):
+        resultado.append(mult_esc_vect((matriz,a)))
+    return resultado
 
 v = [[5,3], [8,2]]
 w = [[3,4], [9,3]]
 
 A = [[(5,3), (8,2)], [(4,8), (6,9)], [(2,5), (3,1)]]
 B = [[(6,4), (4,7)], [(8,3), (9,8)], [(7,1), (9,6)]]
-add_matrices(A,B);
+mult_esc_matriz(A,4);
